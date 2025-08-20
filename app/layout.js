6f6c43cb4b717cfdcd583791649cc5eeec8e4fc2
Vedant-0102 <vedantshinde164@gmail.com>
2025-08-20 21:29:04 +0530

    Add: Convex backend Setup

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,30 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Splitzz",
-  description: "Split your Expense Easily",
+  title: "Splitr",
+  description: "The smartest way to split expenses with friends",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>
-        <Header/>
-        <main className="min-h-screen">{children}</main>
+      <head>
+        <link rel="icon" href="/logos/logo-s.png" sizes="any" />
+      </head>
+      <body className={`${inter.className}`}>        
+          <ConvexClientProvider>
+            <Header />
+            <main className="min-h-screen">
+              
 
+              {children}
+            </main>
+          </ConvexClientProvider>
       </body>
     </html>
   );
