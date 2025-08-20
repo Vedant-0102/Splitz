28c831b10fdf796f13689f343bcecff1e61cb39d
Vedant-0102 <vedantshinde164@gmail.com>
2025-08-20 18:57:47 +0530

    Add: Font and Header comp

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>Hello
-      <br></br>
+    <br></br>
     <Button>Hello</Button>
     </div>
   );
---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Splitzz</div>
+  )
+}
+
+export default Header
