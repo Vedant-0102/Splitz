0621802cf9f9368cf1fc5e55e183c04514d1b016
Vedant-0102 <vedantshinde164@gmail.com>
2025-08-23 03:09:01 +0530

    Landing page routes hooks Complete

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 import { ConvexClientProvider } from "@/components/convex-client-provider";
 import Header from "@/components/Header";
-import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,15 +18,19 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/logos/logo-s.png" sizes="any" />
       </head>
-      <body className={`${inter.className}`}>        
-          <ClerkProvider>
+      <body className={`${inter.className}`}>
+        <ClerkProvider
+          publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+        >
           <ConvexClientProvider>
             <Header />
             <main className="min-h-screen">
+              <Toaster richColors />
+
               {children}
             </main>
           </ConvexClientProvider>
-          </ClerkProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
